1cf9281cfd00eaec36b9eb96571dd85eb2b12a07
zdrossman <zachary.drossman@gmail.com>
2014-10-15 11:12:22 -0400

    Cleanup complete

---
+++ Pods/Target Support Files/Pods-locationTrivia-tableviewsTests-Swizzlean/Pods-locationTrivia-tableviewsTests-Swizzlean-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_locationTrivia_tableviewsTests_Swizzlean : NSObject
+@end
+@implementation PodsDummy_Pods_locationTrivia_tableviewsTests_Swizzlean
+@end
