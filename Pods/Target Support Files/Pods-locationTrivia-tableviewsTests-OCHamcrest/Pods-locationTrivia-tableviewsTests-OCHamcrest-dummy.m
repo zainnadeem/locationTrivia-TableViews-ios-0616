cde2dc9882809466c0d6bdd8a607d4586b94dbc0
markedwardmurray <markedwardmurray@gmail.com>
2015-10-09 11:26:54 -0400

    pods: update xcode 7, target: ios 8.0

--- Pods/Target Support Files/Pods-locationTrivia-tableviewsTests-OCHamcrest/Pods-locationTrivia-tableviewsTests-OCHamcrest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_locationTrivia_tableviewsTests_OCHamcrest : NSObject
-@end
-@implementation PodsDummy_Pods_locationTrivia_tableviewsTests_OCHamcrest
-@end
--- Pods/Target Support Files/Pods-locationTrivia-tableviewsTests-Swizzlean/Pods-locationTrivia-tableviewsTests-Swizzlean-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_locationTrivia_tableviewsTests_Swizzlean : NSObject
-@end
-@implementation PodsDummy_Pods_locationTrivia_tableviewsTests_Swizzlean
-@end
